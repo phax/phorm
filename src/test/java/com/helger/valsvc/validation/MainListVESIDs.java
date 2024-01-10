c670bc739c172497d18cf9ae1071c0e986872c28
Philip Helger <philip@helger.com>
2024-01-10 21:54:21 +0100

    Added tool to list all VESIDs

---
+++ src/test/java/com/helger/valsvc/validation/MainListVESIDs.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright (C) Philip Helger
+ *
+ * All rights reserved.
+ */
+package com.helger.valsvc.validation;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MainListVESIDs
+{
+  private static final Logger LOGGER = LoggerFactory.getLogger (MainListVESIDs.class);
+
+  public static void main (final String [] args)
+  {
+    AppValidator.getAllVESSorted ()
+                .forEach (x -> LOGGER.info (x.getID ().getAsSingleID () + " - " + x.getDisplayName ()));
+  }
+}
